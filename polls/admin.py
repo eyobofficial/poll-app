4dc160f57f720fe25716ee70ded97c1a918cd1d3
eyob tariku <eyobtariku@gmail.com>
2018-03-08 14:43:07 +0300

    Register models to admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+# Import models
+from . import models
+
+admin.site.site_header = 'Polling App'
+admin.site.site_title = 'Polling App'
+
+
+# Register Question Model
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'published_recently', )
+
+
+# Register Choice Model
+@admin.register(models.Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'votes', 'question', )
